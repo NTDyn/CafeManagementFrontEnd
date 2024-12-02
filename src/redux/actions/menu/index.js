82e493aff01e724f113229bd822db7bc93c7d3cc
TranTien050820 <trantien5.8.2000@gmail.com>
2024-12-02 18:43:01 +0700

    Squashed commit of the following:
    commit 9d5b087cfc402aa703a8f8556fa3c4787aa762cb
    Author: TranTien050820 <trantien5.8.2000@gmail.com>
    Date:   Mon Dec 2 18:41:27 2024 +0700
    
        up
    
    commit fe5683bb47c73a23bf5575a4716b12f38e0aead3
    Author: TranTien050820 <trantien5.8.2000@gmail.com>
    Date:   Mon Dec 2 11:23:36 2024 +0700
    
        Squashed commit of the following:
    
        commit cec525e8b4deeb8c8db8ab59443e4e68c813ddac
        Merge: 0b94ce3 c898593
        Author: NTDyn <duyenthuy843@gmail.com>
        Date:   Sun Nov 24 20:12:58 2024 +0700
    
            up
    
        commit c8985930da5aa30ae95e7e7edf3823b940f00b36
        Author: NTDyn <duyenthuy843@gmail.com>
        Date:   Sun Nov 24 20:07:43 2024 +0700
    
            product commit
    
        commit 0b94ce30cdc28663106de456a05fbf8f63bd65a3
        Author: TranTien050820 <trantien5.8.2000@gmail.com>
        Date:   Sun Nov 24 20:06:11 2024 +0700
    
            Squashed commit of the following:
    
            commit b463f6689aafe4d5b884c8005d1166937a1d09ff
            Author: TranTien050820 <trantien5.8.2000@gmail.com>
            Date:   Thu Nov 21 17:29:48 2024 +0700
    
                up
    
    commit b463f6689aafe4d5b884c8005d1166937a1d09ff
    Author: TranTien050820 <trantien5.8.2000@gmail.com>
    Date:   Thu Nov 21 17:29:48 2024 +0700
    
        up

---
+++ src/redux/actions/menu/index.js
@@ -0,0 +1,33 @@
+import { fetchAPI } from "../../../api";
+
+export const getMenu = (_id = -1) => {
+    let url = "/api/Menu";
+    if(_id !== -1){
+        url += `?MenuID=${_id}`;
+    }
+    return async dispatch => {
+      await dispatch({type: "API_REQUEST", typeRequest: 1});
+      await Promise.all([
+        fetchAPI(url)
+      ]).then(
+        response => {
+          let i = 0;
+          let types = ["REFETCH_MENU"];
+          for (const result of response) {
+            if(!result.length){
+              dispatch({type: "API_FAILURE", status: '', message: ''});
+            }else{
+              dispatch({type: "API_SUCCESS", status: '', message: ''});
+              dispatch({type: types[i], data: result});
+            }
+            i++;
+          }
+        }
+      )
+      .catch(
+        error => {
+          dispatch({type: "API_FAILURE", status: -1, message: error.message})
+        }
+      );
+    }
+  }
